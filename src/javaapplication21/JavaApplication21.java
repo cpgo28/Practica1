5a44e42b90f25715dd539cb4bbdb66a55da1998f
alumno <alumno@LIMSFLAB04-07D.system-uap.adm>
2017-09-14 20:40:04 -0500

    1practica-Gonzales

---
+++ src/javaapplication21/JavaApplication21.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication21;
+
+import java.util.Scanner;
+
+
+/**
+ *
+ * @author alumno
+ */
+public class JavaApplication21 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner input = new Scanner(System.in);
+       
+       
+       //mostrar mensaje
+       System.out.println(**);
+       System.out.print("INGRESE UN AÑO");
+       
+       int año=input.nextInt();
+       System.out.print("INGRESE UN MES: ");
+       String mes = input.next();
+       
+       boolean añoBisiesto=
+       
+       
+
